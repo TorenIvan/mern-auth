3296438778fb6e7dd965d6fd114cdd9033971f9b
TorenIvan <Vaggelisshmos@gmail.com>
2021-03-30 21:19:58 +0300

    2 ways to serve the home page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const router  = express.Router();
 const path    = require('path');
 
-const  router = express.Router();
+/* GET home page. */
+router.use('/', express.static(path.resolve('client/public')))
+/* OR */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.resolve('client/public/index.html'));
+});
 
-router.use('/', express.static(path.resolve('../../client/publix' + '/index.html')));
-
+/* Export router */
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,19 +3,15 @@
 
 /* Basic Modules */
 const express = require('express');
-const path    = require('path');
 
 /* Custom modules */
-const routes  = require('./routes/index.js')
+const indexRouter  = require('./routes/index.js')
 
 /* Creates app, specifies port */
-const app = express();
+const app  = express();
 const PORT = process.env.PORT || 5008;
 
-/* Serve static files */
-app.use(express.static(path.join(__dirname, '../client/public')));
-
 /* Router usage */
-app.use('/', routes);
+app.use('/', indexRouter);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
