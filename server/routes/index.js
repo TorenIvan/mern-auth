3096addc1af228fa1fbde518131f827b0bc681f3
TorenIvan <Vaggelisshmos@gmail.com>
2021-04-01 19:55:18 +0300

    Basic functionality of routers1

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,5 +9,32 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.resolve('client/public/index.html'));
 });
 
+router.post('/register', (req, res, next) => {
+  /* Security check */
+  /* Check if user with same name already exists */
+  /* Hash password */
+  /* Update db */
+});
+
+router.post('/login', (req, res, next) => {
+
+});
+
+router.post('/signout', (req, res, next) => {
+
+});
+
+router.post('/profile', (req, res, next) => {
+
+});
+
+router.post('/edit', (req, res, next) => {
+
+});
+
+router.post('/upload', (req, res, next) => {
+
+});
+
 /* Export router */
 module.exports = router;
