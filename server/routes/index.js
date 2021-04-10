4bfcc822ccd562d87756233159ba59598acdeaeb
Toren Ivan <Vaggelisshmos@gmail.com>
2021-04-10 13:51:19 +0300

    More secure handling; also validator usage

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const router  = express.Router();
-const path    = require('path');
+const express    = require('express');
+const router     = express.Router();
+const path       = require('path');
+const userDB     = require('../../models/User');
+const validateR  = require('../../validations/register');
+const validateL  = require('../../validations/login');
+const bcrypt     = require('bcrypt');
+const sanitize   = require('mongo-sanitize');
 
+/* In order to generate the salt to hash the password */
+const saltRounds = 10;
 /* GET home page --> version 1 */
 router.use('/', express.static(path.resolve('client/public')))
 /* GET home page --> version 2 */
@@ -11,15 +18,33 @@ router.get('/', function(req, res) {
 
 router.post('/register', (req, res) => {
   	/* Get request data using object destructuring */
-	const {email, password} = req.body;
+	var data = {email, password} = req.body;
+
+	/* Validate Data; return errors if exist*/
+	var valData = validateR.validateRegister(data).isValid;
+	if(!valData.isValid) return res.json({errors: valData.errors});
 	
-	//Security check done in the server.js file using helmet for xss etc
-  	
+	//Security check done in the server.js file using helmet, xss-clean, rate-limit, hpp
+  	/* Although the above is correct; we need to pay attention for other attacks like nosqli */
+	/* Sanitize input always */
+	email    = sanitize(email);
+	password = sanitize(password);
+
 	/* Check if user with same name already exists */
+  	userDB.User.findOne({email: email}, (err, user) => {
+		if(err) console.log('error during select user');
+		var userExists = true;
+		if(!user) userExists = false;
+		else return res.json({userExists: userExists});
+	})
+	/* Hash password by generating the salt and the combine it with password*/
+	bcrypt.genSalt(saltRounds, (err, salt) => {
+		bcrypt.hash(password, salt, (err, hash) => {
+			/* Insert user to db */
+		})
+	})
   	
-	/* Hash password */
-  	
-	/* Update db */
+
 });
 
 router.post('/login', (req, res) => {
--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const xss-clean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 /* Custom Modules */
 const indexRouter  = require('./routes/index.js')
@@ -18,8 +21,23 @@ mongoose.connect(db, { useNewUrlParser: true })
 
 const PORT = process.env.PORT || 5008;
 
-/* Router usage */
+// Security Middleware Handling
+/* Security Headers */
 app.use(helmet());
+/* XSS Prevention */
+app.use(xss-clean());
+/* HTTP Parameter Pollution Prevention */
+app.use(hpp());
+/* Limiting package rate, prevent DDoS */
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, 	//10 minutes
+	max: 10000,			//10.000 requests
+});
+app.use(limiter);
+// End of Security Middleware Handling
+
+
+/* Router usage */
 app.use('/', indexRouter);
 
 
